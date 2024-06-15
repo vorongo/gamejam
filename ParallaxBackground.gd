69cf8a589891f9af77e121df0d854aea982293de
vorongo <6dima6manukyan6@gmail.com>
2024-06-16 01:21:23 +0200

    numeros2

--- ParallaxBackground.gd
+++ ParallaxBackground.gd
@@ -1,12 +1,27 @@
 extends ParallaxBackground
 
-@export var speed = 100
+# Коэффициент скорости для параллакса (можно настроить по вашему усмотрению)
+var parallax_speed = 0.15
 
-# Called when the node enters the scene tree for the first time.
+# Сохраняем размер экрана для вычислений
+var screen_size = Vector2()
+
 func _ready():
-	pass # Replace with function body.
+	# Получаем размер экрана и конвертируем в Vector2
+	var viewport_size = get_viewport().size
+	screen_size = Vector2(viewport_size.x, viewport_size.y)
+	# Подписываемся на обновления процесса
+	set_process(true)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	scroll_offset.x -= delta * speed
+	# Получаем позицию мыши в мировых координатах
+	var mouse_pos = get_viewport().get_mouse_position()
+	
+	# Вычисляем смещение параллакса на основе позиции мыши
+	var base_offset = (mouse_pos - screen_size / 2) * parallax_speed
+	
+	# Устанавливаем смещение параллакса для каждого слоя
+	for layer in get_children():
+		if layer is ParallaxLayer:
+			var layer_parallax_speed = layer.motion_scale * parallax_speed
+			layer.motion_offset = base_offset * layer_parallax_speed
