19705f28f93741b55948a61a9dd2bc4e003395e3
vorongo <6dima6manukyan6@gmail.com>
2024-06-17 15:09:47 +0200

    d

--- bat.gd
+++ bat.gd
@@ -1,13 +1,16 @@
 extends CharacterBody2D
 
 
-var SPEED = 300.0
+var SPEED = 500.0
 const JUMP_VELOCITY = -400.0
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	$Timer.start()
+	$run.play()
 
 func _physics_process(delta):
 	velocity.x += SPEED * delta
@@ -17,11 +20,15 @@ func _physics_process(delta):
 	move_and_slide()
 
 func  dead():
+	$run.stop()
+	$dead.play()
 	SPEED = 0
 	$CollisionShape2D.disabled = true
 	$AnimationPlayer.play("dead")
 	$Timer.start()
 	TimerState.ghost_d()
+	await $AnimationPlayer.animation_finished
+	queue_free()
 
 
 func _on_timer_timeout():
@@ -29,6 +36,6 @@ func _on_timer_timeout():
 
 
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") or body.is_in_group("ghost"):
 		body.dead()
 
